4801291cd456aaa8f963dc1ec39b8def443bfbae
John Moore <jmoore@netazoic.com>
2017-06-25 17:45:10 -0700

    site with logo upper left

--- src/local/com/netazoic/fto/FTO.java
+++ src/local/com/netazoic/fto/FTO.java
@@ -17,20 +17,44 @@ import com.netazoic.ent.ServENT;
 
 public class FTO extends ServENT{
 	
-	RouteAction homeHdlr = new Home();
+	RouteAction homeHdlr = new HomeHdlr();
+	RouteAction router = new Router();
 	
-	public enum HOME_ROUTE{
-		home
+	public enum FTO_ROUTE{
+		home("/", FTO_NTP.Home), 
+		about("/about", FTO_NTP.About), 
+		donate("/donate", FTO_NTP.Donate);
+		
+		String routeCode;
+		String routeURL;
+		FTO_NTP routeNTP;
+		
+		FTO_ROUTE(String url, FTO_NTP ntp){
+			routeURL = url;
+			routeCode = this.name();
+			routeNTP = ntp;
+		}
+		
+		static FTO_ROUTE getRoute(String url){
+			for (FTO_ROUTE rte : FTO_ROUTE.values()){
+				if(rte.routeURL.equals(url)){
+					return rte;
+				}
+			}
+			return null;
+		}
 	}
 	
 
-	public  enum HOME_NTP{
+	public  enum FTO_NTP{
 		Home("/HOME/Home.hbs","FTO home page"),
+		About("/About.hbs","About FTO"),
+		Donate("/Donate.hbs","Donate through FTO"),
 		;
 	
 		String ntpPath;
 		String desc;
-		HOME_NTP(String t, String d){
+		FTO_NTP(String t, String d){
 			ntpPath = t;
 			desc = d;
 		}
@@ -43,21 +67,45 @@ public class FTO extends ServENT{
 	public void init(ServletConfig config) throws javax.servlet.ServletException {
 			super.init(config);
 
-			defaultRoute = HOME_ROUTE.home.name();
+			defaultRoute = FTO_ROUTE.home.name();
 
-			routeMap.put(HOME_ROUTE.home.name(), homeHdlr);
+			routeMap.put(FTO_ROUTE.home.name(), router);
 	}
 		
-	public class Home extends RouteEO{
+	public class Router extends RouteEO{
+
+		@Override
+		public void routeAction(HttpServletRequest request,
+			HttpServletResponse response, Connection con, HttpSession session)
+						throws IOException, Exception {
+			
+			String url = request.getRequestURI();
+			
+			FTO_ROUTE route = FTO_ROUTE.getRoute(url);
+			
+			FTO_NTP tpl = FTO_NTP.Home;  //default
+			if(route!=null) tpl = route.routeNTP;
+			Map<String,Object> map = new HashMap<String,Object>();
+			//TODO map = getRequestMap(request);
+			parser.parseOutput(map,tpl.ntpPath,response.getWriter());
+
+		}	
+}
+	
+	public class HomeHdlr extends RouteEO{
 
 			@Override
 			public void routeAction(HttpServletRequest request,
 				HttpServletResponse response, Connection con, HttpSession session)
 							throws IOException, Exception {
-				String tPath = HOME_NTP.Home.ntpPath;
+				
+				String url = request.getRequestURI();
+				
+				String route = getRoutePrimary(request);
+				String tPath = FTO_NTP.Home.ntpPath;
 				Map<String,Object> map = new HashMap<String,Object>();
 				//parser.parseOutput(map, tPath, response.getWriter());
-				parser.parseTest(map,tPath,response.getWriter());
+				parser.parseOutput(map,tPath,response.getWriter());
 
 			}	
 	}
---
+++ www/WEB-INF/templates/_include/SlideShow.js
@@ -0,0 +1,56 @@
+<script>
+// Slideshow with granular control
+var slides = [     
+  { url: '/img/slides/children-pray-candelight.jpg', id:'spirit_of_the_lord', alt:'spirit_of_the_lord'},
+  { url: '/img/slides/relief team leader.jpg', id:"good_news", alt:"good_news" },
+  { url: '/img/slides/families run across the road.jpg', id:"freedom", alt:'freedom'},
+  { url: '/img/slides/Grand Teton.jpeg', id:"sight", alt:"sight" },
+  { url: '/img/slides/eliya_first_ranger.jpg',id:"set_opressed_free", alt:"set_opressed_free" },
+  { url: 'img/slides/DERescuesGirlMosul2017.JPG', id:"FTO", alt:'FTO', duration:8000},
+  { url: '/img/slides/black.png', id:'black', alt:'FTO'}
+]
+
+
+var slideCaptions = [
+                     
+      {id: 'spirit_of_the_lord', caption:"The Spirit of the Lord<br/> is upon me"},
+      {id: 'good_news', caption:"To proclaim good news <br/>to the poor"},
+      {id: 'freedom', caption:"To proclaim freedom<br/> for the prisoners"},
+      {id: 'sight', caption:"And recovery of sight <br/> for the blind"},
+      {id: 'set_opressed_free', caption:"To set the opressed free"},
+      {id: 'FTO', caption:"Free The Opressed"}
+      ]
+                     
+$('div.slide-container').backstretch(slides, {duration:7000,fade:4000});                     
+
+$(function(data){
+        var date = new Date();
+        $("#date-stamp").html("<p>Time is: " + date);
+        
+        var caption = slideCaptions[0];
+        switchCaption(caption.caption);
+        
+        $(window).on("backstretch.before", function (e, instance, idx) {
+  			//get the caption
+  			var slide = slides[idx];
+  			var id = slide.id;
+  			var caption = slideCaptions.filter(function(caption){
+  			    return caption.id === id;
+  			});
+  			//debugger;
+  			if(caption.length) switchCaption(caption[0].caption);
+  		});
+  		
+
+});
+
+function switchCaption(captionText){
+	var $cap = $("div.caption-container");
+	$cap.fadeOut(1500, complete);
+
+	function complete(){
+	    $cap.html(captionText).fadeIn(2500);
+	}
+}
+
+</script>
